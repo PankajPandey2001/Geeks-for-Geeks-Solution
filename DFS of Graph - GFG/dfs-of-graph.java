f07fb5db698fda5d25591f73a70f27fc38e6d553
PankajPandey2001 <136225614+PankajPandey2001@users.noreply.github.com>
2023-07-31 19:43:53 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.java
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj =
+                new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < V; i++) adj.add(new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.dfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+/*
+jab bhi graph ka quesiton karo , toh ye dimaag mein rakho ki , graph mein ye maan ke chalo
+ki cycle hogi , aur usi hissab se quesito karo */
+
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        
+        boolean visited[] = new boolean[V] ; 
+        
+        ArrayList<Integer> list = new ArrayList<>() ; 
+        
+        if( V == 1)
+        {
+            list.add(0) ; 
+            return list ; 
+        }
+        
+        dfs( 0 , adj , list , -1 , visited ) ;
+        
+        return list ;
+    }
+    public void dfs( int vertex, ArrayList<ArrayList<Integer>> adj , ArrayList<Integer> list , int parent, boolean visited[] )
+    {
+        
+        if(visited[vertex] == true)
+        return ; 
+        
+        list.add(vertex) ; 
+        
+        
+        visited[vertex] = true ; 
+        
+        
+        ArrayList<Integer> temp = adj.get(vertex) ;
+        
+        for( int i = 0 ; i < temp.size() ; i++)
+        {
+            if( temp.get(i) != parent && temp.get(i) != vertex)
+            {
+                dfs(temp.get(i) , adj , list , vertex , visited) ; 
+            }
+        }
+    }
+}
